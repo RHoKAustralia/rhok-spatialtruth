82022dce274ab7737076c23f1ee01e4a46eb05e5
Jackie Ng <jumpinjackie@gmail.com>
2012-12-02 11:17:25 +1100

    - Implement a bootstrap-based layer switcher - Show popup containing geocoded address - Add an initial view button to navbar - Fix XHR proxy to fix bad google geocode requests

--- index_mobile.php
+++ index_mobile.php
@@ -72,8 +72,6 @@ try
         <style type="text/css">
             #viewer { padding-top: 43px; }
             #theNavbar { margin-bottom: 0px !important; }
-            .olControlLayerSwitcher .olButton { color: white !important; }
-            .olControlLayerSwitcher label.olButton { display: inline !important; }
             .olControlLoadingPanel {
                 background-image:url(img/loading.gif);
                 position: relative;
@@ -127,11 +125,13 @@ try
                 }
             };
 
+            var posPopup = null;
             var sessionId = '<?= $sessionId ?>';
             var map = null;
             var myPositionLayer = null;
             var vicMapLayer = null;
-            var mapAgentUrl = "../mapagent/mapagent.fcgi"
+            var mapAgentUrl = "../mapagent/mapagent.fcgi";
+            var gotoScale = 16;
 
             var metersPerUnit = 1.0;  //value returned from mapguide
             OpenLayers.DOTS_PER_INCH = 96;
@@ -253,12 +253,22 @@ try
                 }
             }
 
+            function initialView() {
+                map.zoomToExtent(new OpenLayers.Bounds(
+                    15175240.12498,
+                    -5058108.0785496,
+                    17209076.573308,
+                    -3893819.2638718
+                ), true);
+            }
+
             function setupMapGuideLayers(oMap) {
                 vicMapLayer = new OpenLayers.Layer.MapGuide("Vicmap - WMS", mapAgentUrl, createMapGuideParams("Library://RHOK/Map/Satellite.MapDefinition", "Vicmap WMS"), createMapGuideOptions());
                 oMap.addLayers([ vicMapLayer ]);
             }
 
             $(document).ready(function() {
+                $("#addressSearch").hide();
                 $(window).bind("resize", onResize);
                 onResize();
                 $("#viewer").width($("div.main").width());
@@ -302,9 +312,9 @@ try
                 map.addLayers([myPositionLayer]);
 
                 map.addControl(new OpenLayers.Control.LoadingPanel());
-                map.addControl(new OpenLayers.Control.LayerSwitcher());
                 map.addControl(new OpenLayers.Control.MousePosition())
                 map.zoomToMaxExtent();
+                checkAddressSearchValidity();
                 mgKeepAlive();
             });
 
@@ -312,37 +322,86 @@ try
                 return map.baseLayer.CLASS_NAME === "OpenLayers.Layer.Google";
             }
 
-            function setMyPosition(lon, lat) {
+            function setMyPositionMarker(lon, lat) {
                 myPositionLayer.removeAllFeatures();
                 var txPt = new OpenLayers.LonLat(lon, lat).transform(
                         new OpenLayers.Projection("EPSG:4326"),
                         map.getProjectionObject()
                     );
+                var geom = new OpenLayers.Geometry.Point(txPt.lon, txPt.lat);
+                myPositionLayer.addFeatures([new OpenLayers.Feature.Vector(geom)]);
+                var pos = new OpenLayers.LonLat(lon, lat).transform(
+                    new OpenLayers.Projection("EPSG:4326"),
+                    map.getProjectionObject()
+                );
+                map.setCenter(pos, gotoScale);
+                return pos;
+            }
+
+            function setMarkerPopup(lon, lat, html) {
+                if (posPopup != null) {
+                    posPopup.destroy();
+                    map.removePopup(posPopup);
+                }
+                posPopup = new OpenLayers.Popup.FramedCloud("Popup", 
+                    new OpenLayers.LonLat(lon, lat), null,
+                    html, null,
+                    true // <-- true if we want a close (X) button, false otherwise
+                );
+                map.addPopup(posPopup);
+            }
+
+            function setMyPosition(lon, lat) {
+                var pos = setMyPositionMarker(lon, lat);
                 if (isGoogleLayerActive()) {
                     //NOTE: 2500 daily request limit for API key-less operations
                     //and only can be used in conjunction with a Google map being displayed
                     var url = OpenLayers.ProxyHost + encodeURIComponent("http://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lon + "&sensor=true");
                     $.getJSON(url, function(data, status, xhr) {
                         if (data.status === "OK") {
-                            if (data.results.length > 0)
-                                console.log("Found: " + data.results[0].formatted_address);
+                            if (data.results.length > 0) {
+                                setMarkerPopup(pos.lon, pos.lat, "<strong>Address:</strong> " + data.results[0].formatted_address)
+                            }
                         } else {
                             alert("Geocode request failed: " + data.status)
                         }
                     });
                 }
-                var geom = new OpenLayers.Geometry.Point(txPt.lon, txPt.lat);
-                myPositionLayer.addFeatures([new OpenLayers.Feature.Vector(geom)])
-                map.setCenter(
-                    new OpenLayers.LonLat(lon, lat).transform(
-                        new OpenLayers.Projection("EPSG:4326"),
-                        map.getProjectionObject()
-                    ), 16
-                );
             }
 
             function populateBaseLayers() {
-                alert("populatin'");
+                var basehtml = "";
+                var overhtml = "";
+                for (var i = 0; i < map.layers.length; i++) {
+                    var layer = map.layers[i];
+                    if (layer.isBaseLayer) {
+                        if (layer.getVisibility()) {
+                            basehtml += "<div class='controls'><label class='radio'><input type='radio' name='baselayername' checked='checked' value='" + layer.name + "' /><i class='icon-reorder'></i> " + layer.name + "</label></div>";
+                        } else {
+                            basehtml += "<div class='controls'><label class='radio'><input type='radio' name='baselayername' value='" + layer.name + "' /><i class='icon-reorder'></i> " + layer.name + "</label></div>";
+                        }
+                    } else {
+                        if (layer == myPositionLayer) //Skip this layer
+                            continue;
+                        if (layer == vicMapLayer) { //You're treated specially
+                            for (var vlName in vicMapLayers) {
+                                var vlLayer = vicMapLayers[vlName];
+                                if (vlLayer.visible) {
+                                    overhtml += "<div class='controls'><label class='checkbox'><input data-vicmap-layer-name='" + vlName + "' type='checkbox' checked='checked' value='" + vlName + "' /><i class='icon-reorder'></i> " + vlName + "</label></div>";
+                                } else {
+                                    overhtml += "<div class='controls'><label class='checkbox'><input data-vicmap-layer-name='" + vlName + "' type='checkbox' value='" + vlName + "' /><i class='icon-reorder'></i> " + vlName + "</label></div>";
+                                }
+                            }
+                        } else {
+                            if (layer.getVisibility()) {
+                                overhtml += "<div class='controls'><label class='checkbox'><input type='checkbox' checked='checked' value='" + layer.name + "' /><i class='icon-reorder'></i> " + layer.name + "</label></div>";
+                            } else {
+                                overhtml += "<div class='controls'><label class='checkbox'><input type='checkbox' value='" + layer.name + "' /><i class='icon-reorder'></i> " + layer.name + "</label></div>";
+                            }
+                        }
+                    }
+                }
+                $("#baseLayerList").html(basehtml + overhtml);
             }
 
             function populateDataFeeds() {
@@ -351,6 +410,59 @@ try
                     html += "<div class='controls'><label class='checkbox'><input type='checkbox' value='" + name + "' /><i class='icon-rss'></i> " + name + "</label></div>";
                 }
                 $("#dataFeedList").html(html);
+            }
+
+            function findLayerByName(layerName) {
+                for (var i = 0; i < map.layers.length; i++) {
+                    if (map.layers[i].name === layerName) {
+                        return map.layers[i];
+                    }
+                }
+                return null;
+            }
+
+            function checkAddressSearchValidity() {
+                if (isGoogleLayerActive())
+                    $("#addressSearch").show();
+                else
+                    $("#addressSearch").hide();
+            }
+
+            function applyLayerVisibility() {
+                notify(MessageType.INFO, "Applying layer visibility");
+                var showVicMapLayers = [];
+                var hideVicMapLayers = [];
+                $("input", "#baseLayerList").each(function(i, e) {
+                    var el = $(e);
+                    if (typeof(el.attr("data-vicmap-layer-name")) != 'undefined') {
+                        var oldVal = vicMapLayers[el.val()].visible;
+                        if (oldVal != el.is(":checked")) {
+                            vicMapLayers[el.val()].visible = el.is(":checked");
+                            if (el.is(":checked"))
+                                showVicMapLayers.push(vicMapLayers[el.val()].objid);
+                            else
+                                hideVicMapLayers.push(vicMapLayers[el.val()].objid)
+                        }
+                    } else {
+                        var layer = findLayerByName(el.val());
+                        if (layer != null)
+                            layer.setVisibility(el.is(":checked"));
+
+                        if (layer.getVisibility() && layer.isBaseLayer)
+                            map.setBaseLayer(layer);
+                    }
+                });
+                if (showVicMapLayers.length > 0 && hideVicMapLayers.length > 0) {
+                    var params = {
+                        ts : (new Date()).getTime(),  //add a timestamp to prevent caching on the server
+                        showLayers : showVicMapLayers.length > 0 ? showVicMapLayers.join(",") : null,
+                        hideLayers : hideVicMapLayers.length > 0 ? hideVicMapLayers.join(",") : null
+                    };
+                    vicMapLayer.mergeNewParams(params);
+                }
+                checkAddressSearchValidity();
+                notify(MessageType.SUCCESS, "Layer visibility changed");
+                $("#viewer").focus();
             }
 
             function applyDataFeeds() {
@@ -386,6 +498,43 @@ try
                     map.zoomToExtent(bounds, true);
                     notify(MessageType.SUCCESS, "Data Feeds Applied");
                 }
+                $("#viewer").focus();
+            }
+
+            function performAddressQuery() {
+                if (isGoogleLayerActive()) {
+                    var addr = $("#qryAddress").val();
+                    notify(MessageType.INFO, "Performing Address Query");
+                    //NOTE: 2500 daily request limit for API key-less operations
+                    //and only can be used in conjunction with a Google map being displayed
+                    var url = OpenLayers.ProxyHost + encodeURIComponent("http://maps.googleapis.com/maps/api/geocode/json?address=" + addr + "&sensor=true");
+                    $.getJSON(url, function(data, status, xhr) {
+                        if (data.status === "OK") {
+                            if (data.results.length > 0) {
+                                var res = data.results[0];
+                                if (typeof(res.geometry) != 'undefined') {
+                                    notify(MessageType.INFO, "Going to first result");
+                                    var lonlat = new OpenLayers.LonLat(
+                                        res.geometry.location.lng,
+                                        res.geometry.location.lat
+                                    ).transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject());
+                                    setMyPositionMarker(res.geometry.location.lng, res.geometry.location.lat);
+                                    map.setCenter(lonlat, gotoScale);
+                                }
+                            } else {
+                                notify(MessageType.DANGER, "Specifed address query returned no results");
+                            }
+                        } else if (data.status == "ZERO_RESULTS") {
+                            notify(MessageType.DANGER, "Specifed address query returned no results");
+                        } else {
+                            notify(MessageType.DANGER, "Google Geocoding API Error: " + data.status);
+                        }
+                    });
+                } else {
+                    alert("Can only search addresses with a Google layer visibile. Sorry, Terms of Service requires it");
+                }
+                $("#viewer").focus();
+                return false;
             }
 
             function myLocation() {
@@ -408,13 +557,17 @@ try
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </a>
-              <a class="brand" href="#">RHoK</a>
+              <a class="brand" href="#">RHoK - Spatial Truth</a>
               <div class="nav-collapse">
                 <ul class="nav">
-                  <li><a href="javascript:myLocation()"><i class="icon-screenshot icon-white"></i> My Location</a></li>
+                  <li><a href="javascript:myLocation()"><i class="icon-screenshot"></i> My Location</a></li>
+                  <li><a href="javascript:initialView()"><i class="icon-screenshot"></i> Initial View</a></li>
                   <li><a href="#baseLayerModal" data-toggle="modal" onclick="populateBaseLayers()"><i class="icon-align-justify"></i> Layers</a></li>
                   <li><a href="#dataFeedsModal" data-toggle="modal" onclick="populateDataFeeds()"><i class="icon-rss"></i> Data Feeds</a></li>
                 </ul>
+                <form id="addressSearch" class="navbar-search pull-right" onsubmit="return performAddressQuery()">
+                  <input type="text" id="qryAddress" class="search-query" placeholder="Address Search">
+                </form>
               </div><!-- /.nav-collapse -->
             </div><!-- /.container -->
           </div><!-- /navbar-inner -->
@@ -429,10 +582,12 @@ try
             <h3 id="baseLayerModalLabel"><i class="icon-align-justify"></i> Base Layers</h3>
           </div>
           <div class="modal-body">
-            <p>One fine body…</p>
+            <p>Choose the layers you want to see on this map</p>
+            <div class="control-group" id="baseLayerList">
+            </div>
           </div>
           <div class="modal-footer">
-            <button class="btn" data-dismiss="modal" aria-hidden="true"><i class="icon-ok"></i> Apply</button>
+            <button class="btn" data-dismiss="modal" aria-hidden="true" onclick="applyLayerVisibility()"><i class="icon-ok"></i> Apply</button>
           </div>
         </div>
         <!-- Data Feeds -->
--- xhrproxy.php
+++ xhrproxy.php
@@ -3,5 +3,16 @@
  * xhrproxy.php - A simple pass-through proxy for cross-domain XHR requests
  */
 
-echo file_get_contents(urldecode($_GET['url']));
+function curl($url){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return $data;
+}
+
+$reqUrl = urldecode($_GET["url"]);
+echo curl(str_replace(" ", "+", $reqUrl));
+
 ?>
